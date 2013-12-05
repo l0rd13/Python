94541d90f2b7d4ef530bb28a80bbe7878440e8ea
l0rd13 <l0rd13@mail.ru>
2013-12-05 00:05:25 -0800

    программа расчёта гипотенузы

---
+++ tringle/tringle.py
@@ -0,0 +1,6 @@
+import math
+print ('------------------')
+a = int (input("Введи катет1:"))
+b = int (input("Введи катет2:"))
+print ('Гипотенуза =' , math.sqrt (a*a + b*b) )
+print ('------------------')
